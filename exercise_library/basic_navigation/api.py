eaf289f6acc379a247f2c85c540ff814defd7bd5
Scott Lobdell <slobdell@hearsaycorp.com>
2015-03-27 13:48:49 -0700

    include everything

--- exercise_library/basic_navigation/api.py
+++ exercise_library/basic_navigation/api.py
@@ -6,6 +6,12 @@ from exercise_library.auto_corrector import SpellChecker, AutoCompleter
 from exercise_library.exercise_cacher import ExerciseCacher
 from exercise_library.cloud_search.tasks import save_exercise_to_amazon
 
+from exercise_library.constants import Equipment
+from exercise_library.constants import ExerciseType
+from exercise_library.constants import MuscleGroup
+from exercise_library.constants import Phase
+from exercise_library.constants import WorkoutComponent
+
 
 def render_to_json(response_obj, context={}, content_type="application/json", status=200):
     json_str = json.dumps(response_obj, indent=4)
@@ -23,7 +29,14 @@ def requires_post(fn):
 
 
 def get_exercises(request):
-    return render_to_json(ExerciseCacher().exercises)
+    return render_to_json({
+        'exercises': ExerciseCacher().exercises,
+        'equipment': sorted(Equipment.as_json(), key=lambda d: d['title']),
+        'muscle_groups': sorted(MuscleGroup.as_json(), key=lambda d: d['title']),
+        'phases': sorted(Phase.as_json(), key=lambda p: p['title']),
+        'workout_components': sorted(WorkoutComponent.as_json(), key=lambda w: w['title']),
+        'exercise_types': sorted(ExerciseType.as_json(), key=lambda d: d['title'])
+    })
 
 
 def autocomplete(request):
