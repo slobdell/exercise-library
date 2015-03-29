9943b46b84ce397fed8985a4dcc69f9f40da8f9a
Scott Lobdell <slobdell@hearsaycorp.com>
2015-03-28 18:01:05 -0700

    add muscle tree

--- exercise_library/basic_navigation/api.py
+++ exercise_library/basic_navigation/api.py
@@ -28,11 +28,28 @@ def requires_post(fn):
     return inner
 
 
+def _create_muscle_tree():
+    muscle_tree = MuscleGroup.get_muscle_ids_as_tree()
+    for top_level_muscle, muscle_dict in muscle_tree.items():
+        for low_level_muscle, muscle_id_list in muscle_dict.items():
+            for index in xrange(len(muscle_id_list)):
+                muscle_name = MuscleGroup.get_name_for_id(muscle_id_list[index])
+                muscle_id_list[index] = (muscle_id_list[index], muscle_name)
+
+    # SBL probably better way to do this
+    muscle_tree_as_list = []
+    for top_level_muscle, muscle_dict in muscle_tree.items():
+        muscle_tree_as_list.append([top_level_muscle, list(muscle_dict.items())])
+
+    return muscle_tree_as_list
+
+
 def get_exercises(request):
     return render_to_json({
         'exercises': ExerciseCacher().exercises,
         'equipment': sorted(Equipment.as_json(), key=lambda d: d['title']),
         'muscle_groups': sorted(MuscleGroup.as_json(), key=lambda d: d['title']),
+        'muscle_tree': _create_muscle_tree(),
         'phases': sorted(Phase.as_json(), key=lambda p: p['title']),
         'workout_components': sorted(WorkoutComponent.as_json(), key=lambda w: w['title']),
         'exercise_types': sorted(ExerciseType.as_json(), key=lambda d: d['title'])
